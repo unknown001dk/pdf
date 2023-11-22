60e6d2056e473ed9728a6342dfb5c3decdbc88f5
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2023-11-23 00:01:20 +0530

    header and footer is added

---
+++ php/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../css/output.css">
+  <?php require "../html/header.html" ?>
+</head>
+<body>
+  
+
+  <h1>hello</h1>
+  
+  
+</body>
+<?php require "../html/footer.html" ?>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../pdf/**/*.{html,js}"],
+  content: ["../pdf/**/*.{html,js,php}"],
   theme: {
     extend: {},
   },
