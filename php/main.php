3706c77b244ee262f1f9371861dc795f0253043c
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2023-11-25 21:02:43 +0530

    login && register is added

--- php/main.php
+++ php/main.php
@@ -3,16 +3,45 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>All in One PDF</title>
   <link rel="stylesheet" href="../css/output.css">
-  <?php require "../html/header.html" ?>
+  <?php 
+  require "../html/header.html" 
+  ?>
 </head>
 <body>
-  
-
-  <h1>hello</h1>
-  
-  
+<div 
+class="bg-indigo-100 py-6 md:py-12"
+>
+      <div 
+      class="text-center max-w-2xl mx-auto"
+      >
+        <h1 
+        class="text-3xl md:text-4xl font-medium mb-2"
+        >
+        Get Ready to change your pdf
+        </h1>
+        <button 
+        class="bg-indigo-600 text-white py-2 px-6 rounded-full text-xl mt-6 hover:bg-blue-300"
+        >
+        Get Started
+        </button>
+        <div 
+        class="mt-4 bg-white width=100"
+        >
+          <input 
+          type="file"
+          name="" 
+          id=""
+          class=""
+          >
+      
+        </div>
+      </div>
+  </div> 
 </body>
-<?php require "../html/footer.html" ?>
+
+<?php 
+require "../html/footer.html"
+?>
 </html>
